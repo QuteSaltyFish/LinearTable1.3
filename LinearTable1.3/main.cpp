ad89d3bc743780b34778fbfc99bfd62b632a2c62
QuteSaltyFish <wangmingke@sjtu.edu.cn>
2018-10-27 15:15:59 +0800

    Initial Commit

---
+++ LinearTable1.3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinearTable1.3
+//
+//  Created by Mingke Wang on 2018/10/27.
+//  Copyright © 2018 Mingke Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
